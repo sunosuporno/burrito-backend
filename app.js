8c42e60f1bf2e6b5904a78c424f123a9290a4ac2
sunosuporno <sarkarsuporno36@gmail.com>
2023-02-06 19:29:43 +0530

    added APIs for searching miners in various ways

--- app.js
+++ app.js
@@ -85,4 +85,128 @@ app.post("/insertStakeAmount/:name/:stakeAmount", async (req, res) => {
   }
 });
 
+// get miner with highest stake amount at endpoint /getMinerWithHighestStakeAmount
+
+app.get("/getMinerWithHighestStakeAmount", (req, res) => {
+  pool.query(
+    "SELECT * FROM miner ORDER BY stake_amount DESC LIMIT 1",
+    (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(results.rows);
+    }
+  );
+});
+
+// get miner with stake amount greater than given amount at endpoint /getMinerWithStakeAmountGreaterThan/:stakeAmount
+
+app.get("/getMinerWithStakeAmountGreaterThan/:stakeAmount", (req, res) => {
+  const stakeAmount = req.params.stakeAmount;
+  pool.query(
+    "SELECT * FROM miner WHERE stake_amount > $1",
+    [stakeAmount],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(results.rows);
+    }
+  );
+});
+
+// get miners with a rating greater than given rating at endpoint /getMinerWithRatingGreaterThan/:rating
+
+app.get("/getMinerWithRatingGreaterThan/:rating", (req, res) => {
+  const rating = req.params.rating;
+  pool.query(
+    "SELECT * FROM miner WHERE rating > $1",
+    [rating],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(results.rows);
+    }
+  );
+});
+
+//get miners with rating as 10 at endpoint /getMinerWithRating10
+
+app.get("/getMinerWithRating10", (req, res) => {
+  pool.query("SELECT * FROM miner WHERE rating = 10", (err, results) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(results.rows);
+  });
+});
+
+// get miners with rating and stake amount greater than given amount at endpoint /getMinerWithReputation/:rating/:stakeAmount
+
+app.get("/getMinerWithReputation/:rating/:stakeAmount", (req, res) => {
+  const rating = req.params.rating;
+  const stakeAmount = req.params.stakeAmount;
+  pool.query(
+    "SELECT * FROM miner WHERE rating > $1 AND stake_amount > $2",
+    [rating, stakeAmount],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(results.rows);
+    }
+  );
+});
+
+// get miners in descending order of stake amount at endpoint /getMinersInDescendingOrderOfStakeAmount
+
+app.get("/getMinersInDescendingOrderOfStakeAmount", (req, res) => {
+  pool.query(
+    "SELECT * FROM miner ORDER BY stake_amount DESC",
+    (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(results.rows);
+    }
+  );
+});
+
+// get miners in descending order of rating at endpoint /getMinersInDescendingOrderOfRating
+
+app.get("/getMinersInDescendingOrderOfRating", (req, res) => {
+  pool.query("SELECT * FROM miner ORDER BY rating DESC", (err, results) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(results.rows);
+  });
+});
+
+// get miners in ascending order of stake amount at endpoint /getMinersInAscendingOrderOfStakeAmount
+
+app.get("/getMinersInAscendingOrderOfStakeAmount", (req, res) => {
+  pool.query(
+    "SELECT * FROM miner ORDER BY stake_amount ASC",
+    (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      res.send(results.rows);
+    }
+  );
+});
+
+// get miners in ascending order of rating at endpoint /getMinersInAscendingOrderOfRating
+
+app.get("/getMinersInAscendingOrderOfRating", (req, res) => {
+  pool.query("SELECT * FROM miner ORDER BY rating ASC", (err, results) => {
+    if (err) {
+      console.log(err);
+    }
+    res.send(results.rows);
+  });
+});
+
 app.listen(PORT);
